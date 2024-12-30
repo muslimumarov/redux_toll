bb7539a2ef83f5c5bd6cba232e80e1004a410257
muslimumarov <muslimbekaxmatovich@gmail.com>
2024-12-30 09:52:24 +0500

    ...

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -8,7 +8,7 @@ const TodosItem = ({id, title}) => {
       <div className="">
         <input className='mr-3 ' type="checkbox"  id="flexCheckDefault" />
         <label className="text-black " htmlFor="flexCheckDefault">
-          {title}
+            {title}
         </label>
       </div>
       <button onClick={() => dispatch(removTodo(id))} className='px-3 m-1 rounded-[10px] text-white py-1 bg-red-700'>Remove</button>
